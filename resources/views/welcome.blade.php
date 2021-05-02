84237a77201afedbeefb2c74365556a632b464a1
dinushchathurya <dinushchathurya21@gmail.com>
2021-05-02 09:53:22 +0530

    feat: create application index blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,103 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
+@section('content')
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+<div class="br-pageheader pd-y-15 pd-l-20">
+    <nav class="breadcrumb pd-0 mg-0 tx-12">
+        <a class="breadcrumb-item" href="https:codingtricks.io">Codingtricks</a>
+        <span class="breadcrumb-item active">Demo</span>
+    </nav>
+</div>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+<div class="pd-x-20 pd-sm-x-30 pd-t-20 pd-sm-t-30">
+    <h4 class="tx-gray-800 mg-b-5">All Srilankan Local Authorities</h4>
+    <p class="mg-b-0">Get all Srilankan Municipal Councils, Urban Councils and Pradeshiya Sabha</p>
 
-            .full-height {
-                height: 100vh;
-            }
+</div>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+<div class="br-pagebody">
 
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+    <div class="row pd-t-20">
+        <div class="col-lg-4">
+            <div class="form-group has-success mg-b-0">
+                <select class="form-control select2" data-placeholder="Choose your Province" name="province" id="province">
+                    <option value="">Choose Province</option>
+                    @foreach ($provinces as $province )
+                    <option value="{{ $province }}">{{ $province}}</option>
+                    @endforeach
+                </select>
             </div>
         </div>
-    </body>
-</html>
+
+        <div class="col-lg-4 mg-t-20 mg-lg-t-0">
+            <div class="form-group has-warning mg-b-0">
+                <select class="form-control select2" data-placeholder="Choose your District" name="district" id="district">
+                </select>
+            </div>
+        </div>
+
+        <div class="col-lg-4 mg-t-20 mg-lg-t-0">
+            <div class="form-group has-warning mg-b-0">
+                <select class="form-control select2" data-placeholder="Choose Council" name="authority" id="authority">
+                </select>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+@endsection
+
+@section('js')
+<script type="text/javascript">
+    $('select[name="province"]').on('change', function() {
+        var province = $(this).val();
+        if (province) {
+            $.ajax({
+                url: "{{ config('app.url') }}/get/district/province/" + province
+                , type: "GET"
+                , dataType: "json"
+                , beforeSend: function() {
+                    $('#loader').css('visibility', 'visible');
+                }
+                , success: function(data) {
+                    $('select[name="district"]').empty();
+                    $('select[name="district"]').append('<option value="">Select a District</option>');
+                    $.each(data, function(key, value) {
+                        $('select[name="district"]').append('<option value="' + value + '">' + value + '</option>');
+                    });
+                }
+                , complete: function() {
+                    $('#loader').css('visibility', 'hidden');
+                }
+            });
+        }
+    });
+    $('select[name="district"]').on('change', function() {
+        var district = $(this).val();
+        if (district) {
+            $.ajax({
+                url: "{{ config('app.url') }}/get/authority/district/" + district
+                , type: "GET"
+                , dataType: "json"
+                , beforeSend: function() {
+                    $('#loader').css('visibility', 'visible');
+                }
+                , success: function(data) {
+                    $('select[name="authority"]').empty();
+                    $('select[name="authority"]').append('<option value="">Select a Authority</option>');
+                    $.each(data, function(key, value) {
+                        $('select[name="authority"]').append('<option value="' + value + '">' + value + '</option>');
+                    });
+                }
+                , complete: function() {
+                    $('#loader').css('visibility', 'hidden');
+                }
+            });
+        }
+    });
+
+</script>
+
+@endsection
+
