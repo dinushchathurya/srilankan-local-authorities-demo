e5e06240d2183761f4b3ffc3068c39bc3bc3a3f0
dinushchathurya <dinushchathurya21@gmail.com>
2021-05-02 09:56:12 +0530

    feat: create HomeController

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Devninja\University\University;
+use Dinushchathurya\Council\Council;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $provinces = Council::getProvinces();
+        return view('authority')->with([
+            'provinces' => $provinces
+        ]);
+    }
+
+    public function getDistricts($name)
+    {
+        $district = Council::getDistricts($name);
+        return response()->json($district);
+    }
+
+    public function getAuthority($name)
+    {
+        $authority = Council::getAuthorities($name);
+        return response()->json($authority);
+    }
+
+    public function documentation()
+    {
+        return view('pages.documentation');
+    }
+
+}
